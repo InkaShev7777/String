bc53d205099aea597b1a742df4f19de8bfee9ec4
Ilya Shevchenko <illyashev2905@gmail.com>
2021-12-20 23:31:35 +0200

    add 1,2,3,4 function

--- String/String.cpp
+++ String/String.cpp
@@ -8,6 +8,85 @@ int main()
     cout << "VVedite text: ";
     getline(cin, str);
     cout << str << "\n";
+    int kol_razd = 0;
+    int vibor;
+    int kol_bukv = 0;
+    int kol_cifir = 0;
+    do
+    {
+        cout << "\tMenu\n";
+        cout << "1 - Kol.razd.znakov\n";
+        cout << "2 - Kol.bukv\n";
+        cout << "3 - Kol.cifir\n";
+        cout << "4 - Vivod v obr.por i zamenf probelov na tochku\n";
+        cout << "5 - Poisk 'Hello'\n";
+        cout << "0 - Exit\n\n";
+        cout << "Sdelayte svoy vibor: ";
+        cin >> vibor;
+        switch (vibor)
+        {
+        case 1:
+            system("cls");
+            for (int i = 0;i < str.length();i++)
+            {
+                if (str[i] == ' ')
+                {
+                    kol_razd++;
+                }
+            }
+            cout << "Kol.razd: " << kol_razd << "\n";
+            system("pause");
+            system("cls");
+            break;
+        case 2:
+            system("cls");
+            for (int i = 0;i <= str.length();i++)
+            {
+                if (str[i] >= 'A' && str[i] <= 'Z'|| str[i] >= 'a' && str[i] <= 'z')
+                {
+                    kol_bukv++;
+                }
+            }
+            cout << "Kol.bukv" << kol_bukv << "\n";
+            system("pause");
+            system("cls");
+            break;
+        case 3:
+            system("cls");
+            for (int i = 0;i <= str.length();i++)
+            {
+                if (str[i] >= '0' && str[i] <= '9')
+                {
+                    kol_cifir++;
+                }
+            }
+            cout << "Kol.cifir" << kol_cifir << "\n";
+            system("pause");
+            system("cls");
+            break;
+        case 4:
+            system("cls");
+            while (str.find(",") < str.length())
+            {
+                str.replace(str.find(","), 1, ".");
+            }
+            for (int i = 0;i <= str.length();i++)
+            {
+                cout <<str[str.length()-i];
+            }
+            cout << "\n";
+            system("pause");
+            system("cls");
+            break;
+        case 5:
+            system("cls");
+
+            system("pause");
+            system("cls");
+            break;
+           
+        }
+    } while (vibor != 0);
 }
 
 
